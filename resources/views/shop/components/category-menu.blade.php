88687d84bab138050e18de19e43dde43ea01ae50
mahdi-taha <official.mahdi.taha@gmail.com>
2026-08-09 17:43:37 +0300

    fix(storefront): separate category menu scrollports

--- resources/views/shop/components/category-menu.blade.php
+++ resources/views/shop/components/category-menu.blade.php
@@ -12,27 +12,61 @@
 
         <div class="dropdown-menu storefront-category-panel-menu p-0" id="categoryMegaMenu"
             aria-labelledby="categoryMegaMenuToggle" data-category-navigation-desktop>
-            <ul class="storefront-category-desktop-list list-unstyled mb-0" role="list">
-                @forelse ($storefrontCategoryNavigation as $rootCategory)
-                    <li class="storefront-category-menu-item {{ $rootCategory['children'] !== [] ? 'has-children' : '' }}">
-                        <a class="dropdown-item storefront-category-link" href="{{ $rootCategory['url'] }}">
-                            <span>{{ $rootCategory['name'] }}</span>
-                            @if ($rootCategory['children'] !== [])
-                                <i class="fas fa-chevron-right storefront-category-forward-icon" aria-hidden="true"></i>
-                            @endif
-                        </a>
-                        @if ($rootCategory['children'] !== [])
-                            <ul class="dropdown-menu storefront-category-submenu list-unstyled" role="list">
+            <div class="storefront-category-root-scrollport">
+                <ul class="storefront-category-desktop-list list-unstyled mb-0" role="list">
+                    @forelse ($storefrontCategoryNavigation as $rootCategory)
+                        <li class="storefront-category-menu-item {{ $rootCategory['children'] !== [] ? 'has-children' : '' }}">
+                            <a class="dropdown-item storefront-category-link" href="{{ $rootCategory['url'] }}"
+                                @if ($rootCategory['children'] !== [])
+                                    data-category-flyout-trigger="root-{{ $rootCategory['id'] }}"
+                                    aria-controls="category-flyout-root-{{ $rootCategory['id'] }}"
+                                    aria-expanded="false"
+                                @endif>
+                                <span>{{ $rootCategory['name'] }}</span>
+                                @if ($rootCategory['children'] !== [])
+                                    <i class="fas fa-chevron-right storefront-category-forward-icon" aria-hidden="true"></i>
+                                @endif
+                            </a>
+                        </li>
+                    @empty
+                        <li class="px-3 py-2 text-muted small">{{ __('shop.navigation.no_subcategories') }}</li>
+                    @endforelse
+                </ul>
+            </div>
+
+            <div class="storefront-category-flyout-layer" data-category-flyout-layer>
+                @foreach ($storefrontCategoryNavigation as $rootCategory)
+                    @if ($rootCategory['children'] !== [])
+                        <div class="storefront-category-flyout storefront-category-flyout--level-2"
+                            id="category-flyout-root-{{ $rootCategory['id'] }}"
+                            data-category-flyout="root-{{ $rootCategory['id'] }}">
+                            <div class="storefront-category-level-2-scrollport">
+                                <ul class="storefront-category-desktop-list list-unstyled mb-0" role="list">
+                                    @foreach ($rootCategory['children'] as $childCategory)
+                                        <li class="storefront-category-menu-item {{ $childCategory['children'] !== [] ? 'has-children' : '' }}">
+                                            <a class="dropdown-item storefront-category-link" href="{{ $childCategory['url'] }}"
+                                                @if ($childCategory['children'] !== [])
+                                                    data-category-flyout-trigger="child-{{ $childCategory['id'] }}"
+                                                    aria-controls="category-flyout-child-{{ $childCategory['id'] }}"
+                                                    aria-expanded="false"
+                                                @endif>
+                                                <span>{{ $childCategory['name'] }}</span>
+                                                @if ($childCategory['children'] !== [])
+                                                    <i class="fas fa-chevron-right storefront-category-forward-icon" aria-hidden="true"></i>
+                                                @endif
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+
+                            <div class="storefront-category-flyout-layer storefront-category-flyout-layer--level-3">
                                 @foreach ($rootCategory['children'] as $childCategory)
-                                    <li class="storefront-category-menu-item {{ $childCategory['children'] !== [] ? 'has-children' : '' }}">
-                                        <a class="dropdown-item storefront-category-link" href="{{ $childCategory['url'] }}">
-                                            <span>{{ $childCategory['name'] }}</span>
-                                            @if ($childCategory['children'] !== [])
-                                                <i class="fas fa-chevron-right storefront-category-forward-icon" aria-hidden="true"></i>
-                                            @endif
-                                        </a>
-                                        @if ($childCategory['children'] !== [])
-                                            <ul class="dropdown-menu storefront-category-submenu list-unstyled" role="list">
+                                    @if ($childCategory['children'] !== [])
+                                        <div class="storefront-category-flyout storefront-category-flyout--level-3"
+                                            id="category-flyout-child-{{ $childCategory['id'] }}"
+                                            data-category-flyout="child-{{ $childCategory['id'] }}">
+                                            <ul class="storefront-category-desktop-list list-unstyled mb-0" role="list">
                                                 @foreach ($childCategory['children'] as $grandchildCategory)
                                                     <li>
                                                         <a class="dropdown-item storefront-category-link" href="{{ $grandchildCategory['url'] }}">
@@ -41,16 +75,14 @@
                                                     </li>
                                                 @endforeach
                                             </ul>
-                                        @endif
-                                    </li>
+                                        </div>
+                                    @endif
                                 @endforeach
-                            </ul>
-                        @endif
-                    </li>
-                @empty
-                    <li class="px-3 py-2 text-muted small">{{ __('shop.navigation.no_subcategories') }}</li>
-                @endforelse
-            </ul>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
         </div>
     </nav>
 </div>
